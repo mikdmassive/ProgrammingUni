12f07bc04701627f92ec34128fc505b38236d81b
Mikey Dodson <mikeyleedodson@gmail.com>
2025-09-29 15:24:25 +0100

    task 4 week 2

---
+++ Week2/Week2_4/Week2_4/Program.cs
@@ -0,0 +1,31 @@
+﻿bool loop = true;
+while (loop)
+{
+    Console.Write("Enter a character:");
+    string input = Console.ReadLine();
+    if (input.Length == 1)
+    {
+        //char entered
+        char inputAsChar = Convert.ToChar(input);
+    
+        if (int.TryParse(input, out int number))
+        {
+            Console.WriteLine("You entered a number");
+        }
+        else if (char.IsSymbol(inputAsChar) || char.IsSeparator(inputAsChar) || char.IsPunctuation(inputAsChar))
+        {
+            Console.WriteLine("You entered a symbol");
+        }
+        else if (char.IsLetter(inputAsChar))
+        {
+            Console.WriteLine("You entered a letter");
+        }
+        Console.WriteLine();
+    }
+    else 
+    {
+        Console.WriteLine("Please enter exactly one character.");
+    }
+
+
+}
