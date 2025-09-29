f415b3c58d4d0e50993962f4dce72450a40ec313
Mikey Dodson <mikeyleedodson@gmail.com>
2025-09-29 15:04:29 +0100

    week 2 task 3

---
+++ Week2/Week2_3/Week2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter your age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+switch (age)
+{
+    case > 65:
+        Console.WriteLine("You are a senior.");
+        break;
+    case > 20:
+        Console.WriteLine("You are a adult.");
+        break;
+    case > 12:
+        Console.WriteLine("You are a teen.");
+        break;
+    case >= 0:
+        Console.WriteLine("You are a child.");
+        break;
+    default:
+        Console.WriteLine("Invalid age.");
+        break;
+}
