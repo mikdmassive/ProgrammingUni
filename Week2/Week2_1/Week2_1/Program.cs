54ee909763af6b4aec8563ca10359f87070c5c7c
Mikey Dodson <mikeyleedodson@gmail.com>
2025-09-29 14:36:44 +0100

    Week 2 Q1

---
+++ Week2/Week2_1/Week2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Login AdminLogin = new Login("admin", "w34kPa55w0rd");
+
+Console.Write("Enter your username: ");
+string username = Console.ReadLine();
+Console.Write("Enter your password: ");
+string password = Console.ReadLine();
+if (username == AdminLogin.username && password == AdminLogin.password)
+{
+    Console.WriteLine("Login successful!");
+}
+else
+{
+    Console.WriteLine("Login failed. Invalid username or password.");
+}
+//CLASSES
+class Login
+{
+    public string username;
+    public string password;
+
+    public Login(string username, string password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+}
