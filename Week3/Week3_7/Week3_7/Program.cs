0e03ed31397a709c4489e53decbf08cbdff3bfcb
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-06 15:34:16 +0100

    w3t7

---
+++ Week3/Week3_7/Week3_7/Program.cs
@@ -0,0 +1,28 @@
+﻿string RegistrationToYear(string Registration)
+{
+    string RegYear = Registration.Substring(2, 2);
+    int RegYearInt = int.Parse(RegYear);
+    if (RegYearInt>=50)
+    {
+        int NewRegYearInt = RegYearInt - 50;
+        RegYear = Convert.ToString(NewRegYearInt);
+        if (NewRegYearInt < 10)
+        {
+            RegYear = "0" + RegYear;
+        }
+    }
+  
+    return "20"+RegYear;
+}
+//PROMPTS
+string Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+string PromptRegistrationToYear()
+{
+    return RegistrationToYear(Prompt("Enter your reg plate:"));
+}
+//MAIN
+Console.WriteLine($"Your car was made in {PromptRegistrationToYear()}");
