d06f7bb1673d9127efb1f8d8ae8813c45cbd68ae
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-06 14:45:45 +0100

    task 3

---
+++ Week3/Week3_3/Week3_3/Program.cs
@@ -0,0 +1,38 @@
+﻿string Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+
+//LIST
+List<string> Tasks = new List<string>() {"Wake up","Shower","Brush teeth","Eat breakfast","Go to lectures","Go home","Sleep"};
+
+string WhatIsAfter(string task)
+{
+    string msg = "ERROR OCCURED";
+    if (Tasks.Contains(task))
+    {
+        int newtaskIndex = Tasks.IndexOf(task)+1;
+        if (newtaskIndex < Tasks.Count)
+        {
+            msg = Tasks[newtaskIndex];
+        }
+        else
+        {
+            msg = "There is no task after this one";
+        }
+    }
+    else
+    {
+        msg = "Task not found";
+    }
+    return msg;
+}
+void PromptWhatIsAfter()
+{
+    Console.WriteLine(WhatIsAfter(Prompt("What task do you want to know the next task of?")));
+}
+while (true)
+{
+    PromptWhatIsAfter();
+}
