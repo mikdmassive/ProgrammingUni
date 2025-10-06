ff029f90df9fb7e248c6a88174933d0a0849c3af
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-06 15:21:32 +0100

    w3t6

---
+++ Week3/Week3_6/Week3_6/Program.cs
@@ -0,0 +1,17 @@
+﻿string GetInsideString(string text) 
+{
+    string trimmedstring = text.Trim();
+    return trimmedstring.Substring(1, trimmedstring.Length - 2);
+}
+//PROMPTS
+string Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+string PromptGetInsideString()
+{
+    return GetInsideString(Prompt("Enter a messy message:"));
+}
+//Main
+Console.WriteLine(PromptGetInsideString());
