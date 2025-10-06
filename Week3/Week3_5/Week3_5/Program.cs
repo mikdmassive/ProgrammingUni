5ed0d0105f7052940bac9e0e0c82e6d6d6761a43
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-06 15:16:15 +0100

    w3t5

---
+++ Week3/Week3_5/Week3_5/Program.cs
@@ -0,0 +1,33 @@
+﻿string TruncateStringIfNecessary (string msg,int len)
+{
+    int stringlen = msg.Length;
+    if(len >= 3)
+    {
+        if (stringlen > len)
+        {
+            return msg.Substring(0, len - 3) + "...";
+        }
+        else
+        {
+            return msg;
+        }
+    }
+   else return "ERROR STRING LENGTH LESS THAN 3";
+}
+
+ //PROMPTS
+ string Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+int IntPrompt(string message)
+{
+    return Convert.ToInt32(Prompt(message));
+}
+void PromptTruncateStringIfNecessary()
+{
+    Console.WriteLine(TruncateStringIfNecessary(Prompt("Enter a message"),IntPrompt("Enter the max length")));
+}
+//MAIN
+PromptTruncateStringIfNecessary();
