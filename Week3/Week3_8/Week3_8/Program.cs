ba3424e0786499ced38a325a1c8fa62d12169849
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-06 15:41:52 +0100

    w3t8

---
+++ Week3/Week3_8/Week3_8/Program.cs
@@ -0,0 +1,24 @@
+﻿int CalculateCentury(int year)
+{
+    int Century = year/ 100;
+    Century++;
+
+
+    return Century;
+}
+//PROMPTS
+string Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Console.ReadLine();
+}
+int IntPrompt(string message)
+{
+    return Convert.ToInt32(Prompt(message));
+}
+int PromptCalculateCentury()
+{
+    return CalculateCentury(IntPrompt("Enter a year to calculate its century: "));
+}
+//Main
+Console.WriteLine($"The century is: {PromptCalculateCentury()}");
