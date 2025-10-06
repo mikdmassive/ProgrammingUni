4044b4e74bdf1ad7fb7ae52920bc37c112b738e8
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-06 14:19:35 +0100

    task 1 done

--- Week3/Week3_1/Week3_1/Program.cs
+++ Week3/Week3_1/Week3_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int clamp(int number,int min,int max)
+{
+    if (number < min) return min;
+    else if(number > max) return max;
+    else return number;
+}
+int intprompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void promptclamp()
+{
+    int number = intprompt("Enter a number:");
+    int min = intprompt("Enter the minimum value:");
+    int max = intprompt("Enter the maximum value:");
+    int clampedValue = clamp(number, min, max);
+    Console.WriteLine($"Clamped value: {clampedValue}");
+    Console.WriteLine();
+}
+
+for (int i = 0; i < 3; i++)
+{
+    promptclamp();
+}
