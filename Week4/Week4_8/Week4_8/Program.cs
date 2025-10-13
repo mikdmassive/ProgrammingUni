8144102593badf7f760fb33c9eb8ec3a011a5253
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-13 15:51:23 +0100

    w4t8

---
+++ Week4/Week4_8/Week4_8/Program.cs
@@ -0,0 +1,26 @@
+﻿int SimulateElevator(string txt)
+{
+    int floor = 0;
+    for(int i = 0; i<txt.Length; i++)
+    {
+        if (txt[i] == 'u')
+        {
+            floor++;
+        }
+        else if (txt[i] == 'd')
+        {
+            floor--;
+        }
+    }
+        
+
+    return floor;
+}
+string Prompt(string txt)
+{
+
+    Console.WriteLine(txt);
+    return Console.ReadLine();
+}
+//main
+Console.WriteLine($"You are on floor {SimulateElevator(Prompt("Enter a sequence of characters where \"u\" is up and \"d\" is down:"))}");
