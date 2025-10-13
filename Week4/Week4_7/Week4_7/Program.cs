27b2946ca22fe53dabc1547ec99c85bbcdf54823
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-13 15:37:06 +0100

    w4t7

---
+++ Week4/Week4_7/Week4_7/Program.cs
@@ -0,0 +1,18 @@
+﻿string GetBirthCountry()
+{
+    string birthcountry = Prompt("Please enter your birth country:");
+    if (birthcountry.Length<4)
+    {
+        Console.WriteLine("Please enter atleast 4 characters.\n");
+        birthcountry = GetBirthCountry();
+    }
+    return birthcountry;
+}
+string Prompt(string txt)
+{
+
+    Console.WriteLine(txt);
+    return Console.ReadLine();
+}
+//main
+Console.WriteLine($"Birth Country : {GetBirthCountry()}");
