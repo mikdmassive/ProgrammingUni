d5bd0f9d5a9bec68601fcbfaea6476ab328f1cd5
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-13 14:17:53 +0100

    Update Program.cs

--- Week4/Week4_2/Week4_2/Program.cs
+++ Week4/Week4_2/Week4_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Functions
+int Sum1ToN(int n)
+{
+    int sum = 0;
+    for (int i = 1;i<=n;i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+Console.WriteLine("Enter a number:");
+int amount = Convert.ToInt32(Console.ReadLine());
+int result = Sum1ToN(amount);
+Console.WriteLine($"The sum of numbers from 1 to {amount} (inclusive) is {result}");
