8ee6550ab31a938ab0606b83790334ad6ad6e2d0
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-20 15:49:18 +0100

    w5t8

---
+++ Week5/Week5_8/Week5_8/Program.cs
@@ -0,0 +1,43 @@
+﻿string ValidateAndReturnPhoneNumber(string phoneNumber)
+{
+    string validmessage = "";
+    bool validchars = true;
+    for (int i = 0; i < phoneNumber.Length; i++)
+    {
+        if (!char.IsDigit(phoneNumber[i]))
+        {
+            validchars = false;
+        }
+    }
+    if (validchars)
+    {
+        switch (phoneNumber.Length)
+        {
+            case > 11:
+                validmessage = "The phone number is too long.";
+                break;
+            case < 11:
+                validmessage = "The phone number is too short.";
+                break;
+            default:
+                validmessage = "The phone number is valid: " + CreatePhoneNumber(phoneNumber);
+                break;
+        }
+    }
+    else
+    {
+        validmessage = "The phone number does not contain only numbers";
+    }
+    return validmessage;
+}
+string Prompt(string txt)
+{
+    Console.WriteLine(txt);
+    return Console.ReadLine();
+}
+string CreatePhoneNumber(string phoneNumber)
+{
+    return $"{phoneNumber.Substring(0, 5)}-{phoneNumber.Substring(5, 3)}-{phoneNumber.Substring(8, 3)}";
+}
+//main
+Console.WriteLine(ValidateAndReturnPhoneNumber(Prompt("Enter a phone number (11 characters):")));
