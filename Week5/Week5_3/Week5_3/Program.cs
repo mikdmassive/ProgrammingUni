50c31dbb13affbb623faa2db9c7014b5a1a9f6bf
Mikey Dodson <mikeyleedodson@gmail.com>
2025-10-20 14:43:27 +0100

    w5t3

---
+++ Week5/Week5_3/Week5_3/Program.cs
@@ -0,0 +1,32 @@
+﻿//functions
+bool ValidateAnswer(List<string> acceptedanswers, string answer)
+{
+    bool valid = false;
+    foreach (string acceptedanswer in acceptedanswers)
+    {
+        string upperacceptedanswer = acceptedanswer.ToUpper();
+        if (upperacceptedanswer == answer.ToUpper())
+        {
+            valid = true;
+        }
+    }
+    return valid;
+}
+void RepeatTillValid(List<string> acceptedanswers, string txt)
+{
+    bool valid = false;
+    while (!valid)
+    {
+        string answer = Prompt(txt);
+        valid = ValidateAnswer(acceptedanswers, answer);
+        Console.WriteLine($"{(valid ? "Great Answer!":"Try again..")}");
+    }
+}
+string Prompt(string txt)
+{
+    Console.WriteLine(txt);
+    return Console.ReadLine();
+}
+//main
+List<string> ValidColours = new List<string>() { "Red", "Green", "Blue" };
+RepeatTillValid(ValidColours, "Whats your fav colour?");
